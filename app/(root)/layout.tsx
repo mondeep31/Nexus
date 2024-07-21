ff9d0568b9581add27cb83e58d94f72af5558445
Mondeep Chetry <mondeepchetry027@gmail.com>
2024-07-22 02:38:44 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Nexus",
+  description: "A workspace for your team, powered by Stream Chat and Clerk.",
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
